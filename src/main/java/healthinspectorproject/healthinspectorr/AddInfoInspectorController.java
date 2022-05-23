82633283e5af05d677d8f3b0dbaf150abdff8451
Zolianul <vladcroicu@yahoo.com>
2022-05-23 12:50:19 +0300

    Add Information about inspector functionality addded

---
+++ src/main/java/healthinspectorproject/healthinspectorr/AddInfoInspectorController.java
@@ -0,0 +1,64 @@
+package healthinspectorproject.healthinspectorr;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddInfoInspectorController implements Initializable {
+
+
+
+
+    @FXML
+    private TextField tf_name;
+
+    @FXML
+    private TextField tf_username;
+    @FXML
+    private TextField tf_surname;
+    @FXML
+    private TextField tf_email;
+    @FXML
+    private TextField tf_adress;
+    @FXML
+    private TextField tf_phone;
+
+    @FXML
+    private Button bt_apply;
+    @FXML
+    private Button bt_back;
+
+
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        bt_apply.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(!tf_username.getText().trim().isEmpty() && !tf_name.getText().trim().isEmpty() &&!tf_surname.getText().trim().isEmpty() &&!tf_phone.getText().trim().isEmpty() &&!tf_adress.getText().trim().isEmpty() &&!tf_email.getText().trim().isEmpty()){
+                    DBUtils.addInfo(event,tf_username.getText(),tf_name.getText(),tf_surname.getText(),tf_phone.getText(),tf_adress.getText(),tf_email.getText());
+                }else {
+                    System.out.println("Please fill in all information");
+                    Alert alert= new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please fill in all information");
+                    alert.show();
+
+                }
+
+
+            }
+        });
+        bt_back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "loggedininspector.fxml","InspectorMain",null,null);
+
+            }
+        });
+    }
+}
